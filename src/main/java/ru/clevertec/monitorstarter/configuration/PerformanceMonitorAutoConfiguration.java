f6f187566380d4d32d13130ebf9af8cb20eb3054
Katerina <ykatrinka3@gmail.com>
2024-12-14 12:26:18 +0300

    add starter

--- src/main/java/ru/clevertec/monitorstarter/configuration/PerformanceMonitorAutoConfiguration.java
+++ src/main/java/ru/clevertec/monitorstarter/configuration/PerformanceMonitorAutoConfiguration.java
@@ -1,11 +1,22 @@
 package ru.clevertec.monitorstarter.configuration;
 
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.clevertec.monitorstarter.aop.PerformanceMonitorAspect;
 
-@AutoConfiguration
+@Configuration
 @EnableConfigurationProperties(PerformanceMonitorProperties.class)
-@EnableAutoConfiguration
 public class PerformanceMonitorAutoConfiguration {
+
+    @Configuration
+    public class PerformanceMonitorAspectConfiguration {
+        @Bean
+        @ConditionalOnProperty(value = "performance-monitor.enable", havingValue = "true")
+        public PerformanceMonitorAspect performanceMonitorAspect(PerformanceMonitorProperties properties) {
+            return new PerformanceMonitorAspect(properties);
+        }
+    }
+
 }
